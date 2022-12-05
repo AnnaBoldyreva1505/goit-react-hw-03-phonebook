9bfd8a2c683ce47c5cf7d45a8670883a1f9e0edb
Anna Boldyreva <hannabl1988@gmail.com>
2022-12-05 22:30:06 +0600

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -44,6 +39,23 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
+  // Работа с локальным хранилищем - записала contscts  в локальное хранилище
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  // получила данные из локального хранилища на страницу после перезагрузки
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const pasredContacts = JSON.parse(contacts);
+
+    if (pasredContacts) {
+      this.setState({ contacts: pasredContacts });
+    }
+  }
 
   render() {
     const visibleContacts = this.getVisibleContacts();
